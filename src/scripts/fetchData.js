34dd62c2c316c7066d110dd40342f41dff27134f
Rohan Korake <rohannkorake@gmail.com>
2026-05-13 19:10:20 +0530

    feat: implement loading progress bar and custom UI pages for 404 and unexpected errors

--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -1,8 +1,9 @@
+import { toggleErrorPage } from "./main.js";
 import { renderData } from "./renderData.js";
 const progressBar = document.getElementById("progressBar");
+const searchButton = document.getElementById("searchButton");
 
 export async function fetchData() {
-  const searchButton = document.getElementById("searchButton");
   const data = document.getElementById("data");
   const inputCity = document.getElementById("inputCity");
   const intputForm = document.querySelector("form");
@@ -12,7 +13,8 @@ export async function fetchData() {
 
   intputForm.addEventListener("submit", (e) => {
     e.preventDefault();
-
+    searchButton.disabled = true;
+    startLoading();
     handleFetchRequest();
   });
 }
@@ -21,18 +23,51 @@ export async function fetchData() {
 async function handleFetchRequest() {
   const inputCity = document.getElementById("inputCity");
   let city = inputCity.value;
-
+  toggleErrorPage("cityNotFoundPage", "flex", "hidden");
+  toggleErrorPage("somethingWentWrongPage", "flex", "hidden");
   if (!city) return;
 
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=461e045a3594b58bf341042a8f534f37`
-  );
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=461e045a3594b58bf341042a8f534f37`
+    );
 
-  let body = await response.json();
+    let body = await response.json();
+    stopLoading();
+    searchButton.disabled = false;
 
-  if (response.status == 200) {
-    renderData(response);
+    if (response.status == 200) {
+      renderData(response);
+    } else if (response.status == 404) {
+      toggleErrorPage("cityNotFoundPage", "hidden", "flex");
+    } else {
+      toggleErrorPage("somethingWentWrongPage", "hidden", "flex");
+    }
+  } catch (error) {
+    stopLoading();
+    searchButton.disabled = false;
+    toggleErrorPage("somethingWentWrongPage", "hidden", "flex");
   }
-  console.log(body);
-  console.log(response.status);
 }
+
+// start loader
+function startLoading() {
+  progressBar.style.width = "50%";
+}
+
+// stop loader
+function stopLoading() {
+  progressBar.style.width = "100%";
+
+  setTimeout(() => {
+    progressBar.style.width = "0%";
+  }, 1000);
+}
+
+// handle retry button re-fetch request
+const retrybutton = document.getElementById("retrybutton");
+retrybutton.addEventListener("click", () => {
+  searchButton.disabled = true;
+  startLoading();
+  handleFetchRequest();
+});
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
-import { fetchData } from './fetchData.js';
+import { fetchData } from "./fetchData.js";
 
-document.addEventListener('DOMContentLoaded', () => {
+// entry point of application
+document.addEventListener("DOMContentLoaded", () => {
+  toggleErrorPage("cityNotFoundPage", "flex", "hidden");
+  toggleErrorPage("somethingWentWrongPage", "flex", "hidden");
   fetchData();
 });
+
+// handle error pages visibility
+export function toggleErrorPage(pageName, removeClass, addClass) {
+  pageName = document.getElementById(pageName);
+  pageName.classList.remove(removeClass);
+  pageName.classList.add(addClass);
+}
